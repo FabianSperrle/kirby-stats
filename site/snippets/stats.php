4504abfa6c793eda9dc80cfc6ce65f1b4551de2a
Fabian Sperrle <fabian.sperrle@gmail.com>
2015-03-12 15:29:49 +0100

    some cleaning

--- site/snippets/stats.php
+++ site/snippets/stats.php
@@ -10,17 +10,25 @@ if (!$stats) {
 		exit;
 	}
 }
-$data = $stats->pages()->yaml();
+
+// Get current information
+$pages = $stats->pages()->yaml();
 $dates = $stats->dates()->yaml();
 $date = date("d.m.y");
-$val = (array_key_exists($param, $data)) ? (int) $data[$param]['count'] + 1 : 1;
+
+// Update or create depending on result
+$val = (array_key_exists($param, $pages)) ? (int) $pages[$param]['count'] + 1 : 1;
 $today = (array_key_exists($date, $dates)) ? (int) $dates[$date]['count'] + 1 : 1;
 $total = (!$stats->total_stats_count()->isEmpty()) ? $stats->total_stats_count()->int() + 1 : 1;
-$data[$param] = array('count' => $val);
+
+// Create new data
+$pages[$param] = array('count' => $val);
 $dates[$date] = array('count' => $today);
 $dates = array_slice($dates, -5, 5, true);
+
+// Save
 try {
-	$stats->update(array('pages' => yaml::encode($data), 'dates' => yaml::encode($dates), 'total_stats_count' => $total, ));
+	$stats->update(array('pages' => yaml::encode($pages), 'dates' => yaml::encode($dates), 'total_stats_count' => $total, ));
 } catch (Exception $e) {
 	echo $e->getMessage();
 	exit;
--- site/widgets/stats/stats.php
+++ site/widgets/stats/stats.php
@@ -2,22 +2,28 @@
 return array(
 		'title' => 'Site stats',
 		'html'  => function() {
-		$stats = kirby()->site()->pages()->find('stats');
-		$data = $stats->pages()->yaml();
-		$hits = $stats->total_stats_count()->int();
-		$dates = $stats->dates()->yaml();
-		// Remove one level of nesting
-		$clean = array();
-		$history = array();
-		foreach ($data as $page => $arr) {
-			$clean[$page] = round($arr['count'] / $hits, 2);
-		}
-		foreach ($dates as $date => $arr) {
-			$history[$date] = $arr['count'];
-		}
-		// Sort and keep 5 most important pages
-		arsort($clean);
-		$clean = array_slice($clean, 0, 5, true);
-		return tpl::load(__DIR__ . DS . 'template.php', array('data' => $clean, 'history' => $history));
+			$stats = kirby()->site()->pages()->find('stats');
+			$pages = $stats->pages()->yaml();
+			$dates = $stats->dates()->yaml();
+			$hits = $stats->total_stats_count()->int();
+
+			$clean = array();
+			$history = array();
+			// Remove on nesting level and calculate percentage of total hits
+			// in one go
+			foreach ($pages as $page => $arr) {
+				$clean[$page] = round($arr['count'] / $hits, 2);
+			}
+
+			// Remove one level of nesting for the day-specific hit counters
+			foreach ($dates as $date => $arr) {
+				$history[$date] = $arr['count'];
+			}
+
+			// Sort and keep 5 most important (most accessed) pages
+			arsort($clean);
+			$clean = array_slice($clean, 0, 5, true);
+
+			return tpl::load(__DIR__ . DS . 'template.php', array('data' => $clean, 'history' => $history));
 		}
 		);
